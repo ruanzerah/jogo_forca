12ea965aeb75ef3ede5d183844b25faa81c03172
ruanzerah <ruanlima2023@outlook.com>
2026-03-29 17:39:39 -0300

    feat: version 3.0 - load and save player info using txt   * check if the files exist - limit jackpot checking used letters less than word by half - now the word guess aren't stored in used letters

--- jogo_forca.py
+++ jogo_forca.py
@@ -22,16 +22,19 @@ def criar_forca(lista_secreta, num_tentativas=5):
     letras_utilizadas = []
     while True:
         letra_escolhida = input("Digite uma letra: ")
-        if (letra_escolhida == palavra_secreta) and len(letras_utilizadas) < len(palavra_secreta)/2:
+        if (letra_escolhida == palavra_secreta) and len(letras_utilizadas) < len(palavra_secreta)//2:
             pontuacao += 999
             print(f"JACKPOT! você agora tem {pontuacao} pontos")
             print(f'Você descobriu a palavra: {palavra_secreta}')
             break
-        letras_utilizadas.append(letra_escolhida)
+        else:
+            print("Você não pode mais chutar a palavra.")
         if len(letra_escolhida) > 1:
             num_tentativas -= 1
             print(f'Você tem {num_tentativas} tentativas restantes...')
             continue
+        else:
+            letras_utilizadas.append(letra_escolhida)
         if letra_escolhida not in palavra_secreta:
             num_tentativas -= 1
             print(f'Você tem {num_tentativas} tentativas restantes...')
@@ -48,23 +51,64 @@ def criar_forca(lista_secreta, num_tentativas=5):
         if num_tentativas == 0:
             print('Acabaram suas tentativas')
             break
-
+def carregar_jogo_existente(arquivo):
+    try:
+        with open(f'{arquivo}.txt', "rt") as a:
+            dados = a.read().splitlines()
+    except FileNotFoundError:
+        print("Arquivo nao encontrado")
+    else:
+        dado = ''.join(dados).split(':')
+        n = dado[0]
+        p = int(dado[1])
+        return n, p
+    finally:
+        a.close()
+def salvar_jogo(nome, pontos):
+    try:
+        with open(f'{str(nome).lower()}.txt', "wt+") as a:
+            a.write(f'{nome_jogador}:{pontos}\n')
+    except FileNotFoundError:
+        print("Arquivo nao encontrado")
+    finally:
+        a.close()
+def arquivo_existe(arquivo):
+    try:
+        with open(f'{arquivo}.txt') as a:
+            a.close()
+            return True
+    except FileNotFoundError:
+        return False
 pontuacao = 0
+nome_jogador = None
 print("Bem vindo ao jogo da Forca")
 print('1 - Jogar')
 print('2 - Ver Pontos')
 print('3 - Sair')
+print('4 - Carregar Jogo')
 
 while True:
     entrada = int(input("Digite o numero do oque deseja fazer: "))
     match entrada:
         case 1:
+            nome_j = input("Digite o nome do jogador: ")
+            if nome_j:
+                nome_jogador = nome_j
             tentativas = int(input("Digite a quantidade de tentativas: "))
             criar_forca(carregar_arquivo_lista("lista_palavras.txt"), tentativas)
         case 2:
             print(f'Você tem {pontuacao} pontos')
         case 3:
+            print("Salvando o jogo...")
+            salvar_jogo(nome_jogador, pontuacao)
             break
+        case 4:
+            nome_jogador = input("Digite o nome do jogador: ")
+            if nome_jogador and arquivo_existe(nome_jogador):
+                print(f"Arquivo de save encontrado para o jogador: {nome_jogador}")
+                _, pontuacao = carregar_jogo_existente(nome_jogador)
+            else:
+                print(f"Arquivo de save não encontrado para o jogador: {nome_jogador}")
         case _:
             print("Opções disponiveis: ")
             print('1 - Jogar')
